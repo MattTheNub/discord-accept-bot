0364e2a5537915b5c66d80ee828bbe234d29d784
Matt <mmszek3000@gmail.com>
2018-03-03 19:44:57 -0500

    OOPS (fix using a config file)

--- bin/acceptbot.js
+++ bin/acceptbot.js
@@ -8,12 +8,12 @@ var config = {}
 if (config.config || config.c) {
   let configReader = new ConfigReader(require(config.config || config.c))
   config = configReader.read()
+} else {
+  config.prefix = argv.prefix || argv.p || '/'
+  config.channel = argv.channel || argv.h || 'rules'
+  config.command = argv.command || argv.d || 'accept'
+  config.role = argv.role || argv.r
+  config.token = argv.token || argv.t
 }
-config.prefix = argv.prefix || argv.p || '/'
-config.channel = argv.channel || argv.h || 'rules'
-config.command = argv.command || argv.d || 'accept'
-config.role = argv.role || argv.r
-config.token = argv.token || argv.t
-
 const acceptBot = new AcceptBot(config)
 void acceptBot
